c5131e08566526791292c3e61835dbfdb22996a0
potalzz <tprhkd1607@naver.com>
2024-04-17 17:11:35 +0900

    make rand num

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-const guessNum = "";
+// let answer = String(Math.floor(Math.random() * 10000));
+const answer = [
+  String(Math.floor(Math.random() * 10)),
+  String(Math.floor(Math.random() * 10)),
+  String(Math.floor(Math.random() * 10)),
+  String(Math.floor(Math.random() * 10)),
+];
+
+let ball = 0;
+let strike = 0;
+
+console.log(answer);
+let guessNum = prompt("숫자를 입력해주세요.");
+guessNum = guessNum.split("");
+
+for (const [idx, i] of guessNum.entries()) {
+  if (i === answer[idx]) {
+    strike += 1;
+  }
+  // if ( answer.includes(i)) {
+  //   ball +
+  // }
+}
+/*
+정답을 배열로 저장해서, index와 값이 같으면 strike +1
+정답 배열안에 내가 입력한 숫자가 있는데 index가 다르면 ball + 1
+
+비교방식
+입력 숫자 하나당 정답 배열 돌면서 비교하기.
+*/
